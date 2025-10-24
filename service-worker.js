4be08dc27820f0bcf5e6641c01af96defe1bb023
Ajinkrishna P U <ajinkrishna0001@gmail.com>
2025-10-24 15:20:21 +0530

    fixed fro github pages

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'robot-controller-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',  
-  '/icon-512.png'
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',  
+  'icon-512.png'
 ];
 
 // Install service worker and cache files
